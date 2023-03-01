eaf083d5c78d186ebf056e5e1cd8e0e1d2e4c02a
Adhiana <adhianamastur48@gmail.com>
2023-03-02 05:59:24 +0700

    fix(pkg/notification): comment to prevent error when generate wire

--- pkg/notification/firebase.go
+++ pkg/notification/firebase.go
@@ -1,74 +1,74 @@
 package notification
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"github.com/appleboy/go-fcm"
+// 	"github.com/appleboy/go-fcm"
 
-	"context"
+// 	"context"
 
-	firebase "firebase.google.com/go"
-	"google.golang.org/api/option"
-)
+// 	firebase "firebase.google.com/go"
+// 	"google.golang.org/api/option"
+// )
 
-func init() {
-	ctx := context.Background()
+// func init() {
+// 	ctx := context.Background()
 
-	opt := option.WithCredentialsJSON([]byte{})
-	app, err := firebase.NewApp(ctx, nil, opt)
-	if err != nil {
-		panic(err)
-	}
+// 	opt := option.WithCredentialsJSON([]byte{})
+// 	app, err := firebase.NewApp(ctx, nil, opt)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	client, err := app.Messaging(ctx)
+// 	client, err := app.Messaging(ctx)
 
-	panic(client)
-	// client.
-}
+// 	panic(client)
+// 	// client.
+// }
 
-type FirebaseNotification struct {
-	client *fcm.Client
-}
+// type FirebaseNotification struct {
+// 	client *fcm.Client
+// }
 
-func NewFirebaseNotification(apikey string) (Notification, error) {
-	client, err := fcm.NewClient(apikey)
-	if err != nil {
-		return nil, err
-	}
+// func NewFirebaseNotification(apikey string) (Notification, error) {
+// 	client, err := fcm.NewClient(apikey)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &FirebaseNotification{
-		client: client,
-	}, nil
-}
+// 	return &FirebaseNotification{
+// 		client: client,
+// 	}, nil
+// }
 
-func (n *FirebaseNotification) Send() error {
+// func (n *FirebaseNotification) Send() error {
 
-}
+// }
 
-func init() {
-	// Create the message to be sent.
-	msg := &fcm.Message{
-		To: "sample_device_token",
-		Data: map[string]interface{}{
-			"foo": "bar",
-		},
-		Notification: &fcm.Notification{
-			Title: "title",
-			Body:  "body",
-		},
-	}
+// func init() {
+// 	// Create the message to be sent.
+// 	msg := &fcm.Message{
+// 		To: "sample_device_token",
+// 		Data: map[string]interface{}{
+// 			"foo": "bar",
+// 		},
+// 		Notification: &fcm.Notification{
+// 			Title: "title",
+// 			Body:  "body",
+// 		},
+// 	}
 
-	// Create a FCM client to send the message.
-	client, err := fcm.NewClient("sample_api_key")
-	if err != nil {
-		log.Fatalln(err)
-	}
+// 	// Create a FCM client to send the message.
+// 	client, err := fcm.NewClient("sample_api_key")
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
 
-	// Send the message and receive the response without retries.
-	response, err := client.Send(msg)
-	if err != nil {
-		log.Fatalln(err)
-	}
+// 	// Send the message and receive the response without retries.
+// 	response, err := client.Send(msg)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
 
-	log.Printf("%#v\n", response)
-}
+// 	log.Printf("%#v\n", response)
+// }
